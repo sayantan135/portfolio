172b2ff558f556a48413ab826ef9f417bdc6bf0d
Sayantan <sayantanpal2002@gmail.com>
2024-04-19 19:48:02 +0530

    Update AboutCard.js with final year information

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-          I am an enthusiastic and motivated third-year undergraduate student pursuing a B.Tech in Computer Science Engineering. My expertise lies in web development, where I have experience building web applications using React and proficiency in HTML, CSS, and JavaScript. I also have practical knowledge in cloud computing, working with AWS and Azure to deploy applications and configure scalable architectures. Additionally, I have a keen interest in machine learning and am familiar with various algorithms and techniques. I am currently seeking internships or part-time positions to enhance my skills and contribute to impactful projects. I am excited to collaborate with professionals and fellow enthusiasts to create something extraordinary together.
+          I am an enthusiastic and motivated final year undergraduate student pursuing a B.Tech in Computer Science Engineering. My expertise lies in web development, where I have experience building web applications using React and proficiency in HTML, CSS, and JavaScript. I also have practical knowledge in cloud computing, working with AWS and Azure to deploy applications and configure scalable architectures. Additionally, I have a keen interest in machine learning and am familiar with various algorithms and techniques. I am currently seeking internships or part-time positions to enhance my skills and contribute to impactful projects. I am excited to collaborate with professionals and fellow enthusiasts to create something extraordinary together.
           </p>
           <ul>
             <li className="about-activity">
